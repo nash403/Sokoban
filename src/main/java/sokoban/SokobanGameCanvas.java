b1ff83f32f9058e374346c19d8d2322189b155ce
LucasDelvallet <lucas.delvallet@hotmail.fr>
2015-01-13 21:06:53 +0100

    Added some Sokoban files

---
+++ src/main/java/sokoban/SokobanGameCanvas.java
@@ -0,0 +1,9 @@
+package sokoban;
+
+import gameframework.drawing.GameCanvasDefaultImpl;
+
+public class SokobanGameCanvas extends GameCanvasDefaultImpl{
+	public SokobanGameCanvas(){
+		super();
+	}
+}
