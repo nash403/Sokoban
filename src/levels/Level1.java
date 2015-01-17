bfdf2d0700c053a3be57835e4b2d91839bf0e0e1
nash403 <honore.nintunze@gmail.com>
2015-01-17 16:02:06 +0100

    some changes

--- src/levels/Level1.java
+++ src/levels/Level1.java
@@ -1,8 +1,8 @@
 package levels;
 
-import Entities.Crate;
-import Entities.Player;
-import Entities.Wall;
+import entities.Crate;
+import entities.Player;
+import entities.Wall;
 import gameframework.drawing.GameUniverseViewPortDefaultImpl;
 import gameframework.game.GameData;
 import gameframework.game.GameLevelDefaultImpl;
@@ -16,47 +16,50 @@ public class Level1 extends GameLevelDefaultImpl {
 
 	@Override
 	protected void init() {
-		//universe = this.data.getUniverse();
+		// universe = this.data.getUniverse();
 		this.gameBoard = new GameUniverseViewPortDefaultImpl(data);
-		
-		/*player prend désormé directement un data, pour prendre son canevas et sont moveblocker*/
+
+		/*
+		 * player prend désormé directement un data, pour prendre son canevas et
+		 * sont moveblocker
+		 */
 		universe.addGameEntity(new Player(data, 50, 50));
-		universe.addGameEntity(new Crate(data,100,100));
+		universe.addGameEntity(new Crate(data, 100, 100));
 		createContourWalls();
 		createCourse();
 	}
 
-	protected void createContourWalls(){
+	protected void createContourWalls() {
 		createLeftSideWalls();
 		createBottomSideWalls();
 		createRightSideWalls();
 		createTopSideWalls();
 	}
 
-	public void createLeftSideWalls(){
-		for(int i=0; i<9; i++)
+	public void createLeftSideWalls() {
+		for (int i = 0; i < 9; i++)
 			universe.addGameEntity(new Wall(data.getCanvas(), 0, i));
 	}
-	
-	public void createBottomSideWalls(){
-		for(int i=0; i<9; i++)
+
+	public void createBottomSideWalls() {
+		for (int i = 0; i < 9; i++)
 			universe.addGameEntity(new Wall(data.getCanvas(), i, 9));
 	}
-	
-	public void createRightSideWalls(){
-		for(int i=9; i>0; i--)
+
+	public void createRightSideWalls() {
+		for (int i = 9; i > 0; i--)
 			universe.addGameEntity(new Wall(data.getCanvas(), 9, i));
 	}
-	
-	public void createTopSideWalls(){
-		for(int i=9; i>0; i--)
+
+	public void createTopSideWalls() {
+		for (int i = 9; i > 0; i--)
 			universe.addGameEntity(new Wall(data.getCanvas(), i, 0));
 	}
-	
-	public void createCourse(){
+
+	public void createCourse() {
 		universe.addGameEntity(new Wall(data.getCanvas(), 7, 8));
 		universe.addGameEntity(new Wall(data.getCanvas(), 7, 7));
-		
+
 		universe.addGameEntity(new Wall(data.getCanvas(), 8, 3));
 		universe.addGameEntity(new Wall(data.getCanvas(), 7, 3));
 		universe.addGameEntity(new Wall(data.getCanvas(), 8, 1));
