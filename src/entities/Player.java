76dbc3feb76d9f5da2e1f5dacebf9744e6eea487
DelvalletQuentin <quentin.delvallet@hotmail.fr>
2015-01-18 21:54:46 +0100

    New character and animation
    Added a SpriteManager to do the direction and animation

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -1,30 +1,62 @@
 package entities;
 
+import gameframework.drawing.SpriteManagerDefaultImpl;
 import gameframework.game.GameData;
 import gameframework.motion.GameMovableDriverDefaultImpl;
 import gameframework.motion.MoveStrategyKeyboard;
+import gameframework.motion.SpeedVector;
 import gameframework.motion.overlapping.Overlappable;
 
-public class Player extends SokobanMovable implements Overlappable {
+import java.awt.Graphics;
+import java.awt.Point;
 
-	public Player(GameData data, int x, int y) {
-		//super(data, x, y, "/images/man1.gif", new SpeedVector(new Point(0,0), 1)); ///!\ A d'commentariser apres la maj du framework
-		super(data, x, y, "/images/man1.gif"); ///!\ A retirer apres maj du framework
+public class Player extends SokobanMovable implements Overlappable {
+	
+	protected SpriteManagerDefaultImpl spriteManager;
+
+	public Player(GameData data, int x, int y) {		
+		super(data, x, y, "/images/player4.gif");
 		
-		MoveStrategyKeyboard keyboard = new MoveStrategyKeyboard(false);
-		keyboard.getSpeedVector().setSpeed(data.getConfiguration().getSpriteSize());
-		
+		MoveStrategyKeyboard keyboard = new MoveStrategyKeyboard(false, new SpeedVector(new Point(0,0), spriteSize));	
+	
 		GameMovableDriverDefaultImpl moveDriver = new GameMovableDriverDefaultImpl();
 		moveDriver.setStrategy(keyboard);
 		moveDriver.setmoveBlockerChecker(data.getMoveBlockerChecker());
 		setDriver(moveDriver);
 
 		this.canvas.addKeyListener(keyboard);
+		
+		spriteManager = new SpriteManagerDefaultImpl(image, 32, 6);
+		spriteManager.setTypes("down", "right", "left", "up");
 	}
 
 	@Override
 	public void oneStepMoveAddedBehavior() {
 		
+		Point p = speedVector.getDirection();
+		
+		//Down
+		if(p.x == 0 && p.y > 0)
+			spriteManager.setType("down");
+		
+		//Up
+		if(p.x == 0 && p.y < 0)
+			spriteManager.setType("up");
+		
+		//Left
+		if(p.x < 0 && p.y == 0)
+			spriteManager.setType("left");
+		
+		//Right
+		if(p.x > 0 && p.y == 0)
+			spriteManager.setType("right");
+		
+	}
+
+	@Override
+	public void draw(Graphics g) {
+		spriteManager.draw(g, position);
+		spriteManager.increment();
 	}
 
 }
