47cf63820591d781bfddff4ab43fd2d708d11abb
LucasDelvallet <lucas.delvallet@hotmail.fr>
2015-01-23 13:18:50 +0100

    Added change to have the label with the number of the current level. Added a level. Switched level 5 and 6 Changed classpath.

--- src/game/SokobanGame.java
+++ src/game/SokobanGame.java
@@ -3,7 +3,9 @@ package game;
 import gameframework.game.Game;
 import gameframework.game.GameData;
 import gameframework.game.GameLevel;
+import gameframework.gui.GameStatusBarElement;
 
+import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -19,14 +21,27 @@ public class SokobanGame implements Game, Observer {
 	private SokobanLevel currentPlayedLevel = null;
 
 	protected final GameData data;
-
-	public SokobanGame(GameData data) {
-		this.data = data;
+	protected final ArrayList<GameStatusBarElement<Integer>> windowElements;
+	
+	@SafeVarargs
+	public SokobanGame(GameData gamedata,
+		GameStatusBarElement<Integer>... elements) {
+		this.data = gamedata;
 		data.getEndOfGame().addObserver(this);
+		this.windowElements = new ArrayList<GameStatusBarElement<Integer>>();
+		for(GameStatusBarElement<Integer> element : elements){
+			windowElements.add(element);
+		}
+	}
+	
+	public void newLevel(Integer newLevel){
+		this.windowElements.get(0).getElement().setValue(newLevel);
+		this.windowElements.get(0).update();
 	}
 
 	@Override
 	public void start() {
+		int currentLevel = 1;
 		for (GameLevel level : data.getLevels()) {
 			data.getEndOfGame().setValue(false);
 			if (currentPlayedLevel != null && currentPlayedLevel.isAlive()) {
@@ -34,6 +49,8 @@ public class SokobanGame implements Game, Observer {
 				currentPlayedLevel = null;
 			}
 			currentPlayedLevel = (SokobanLevel) level;
+			newLevel(currentLevel);
+			currentLevel ++;
 			currentPlayedLevel.start();
 			try {
 				currentPlayedLevel.join();
